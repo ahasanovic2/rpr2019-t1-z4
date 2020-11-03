8cbf7048425655d652264f2b15fcb3dff71dbf38
Ahmedin Hasanovic <ahasanovic2@etf.unsa.ba>
2020-11-03 09:52:20 +0100

    Urađene sve 3 klase od temelja do krova

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] korpa = new Artikl[50];
+    private int brojac = 0;
+    public boolean dodajArtikl (Artikl a) {
+        if (brojac >= 50) return false;
+        korpa[brojac] = new Artikl(a.getNaziv(),a.getCijena(),a.getKod());
+        brojac++;
+        return true;
+    }
+
+    public Artikl[] getKorpa() {
+        return korpa;
+    }
+    public Artikl izbaciArtiklSaKodom (String kod) {
+        for (int i = 0; i<brojac;i++)
+        {
+            if (korpa[i].getKod().equals(kod)) {
+                Artikl povrat = new Artikl(korpa[i].getNaziv(),korpa[i].getCijena(),korpa[i].getKod());
+                korpa[i] = null;
+                for (int j = i; j < brojac; j++) korpa[i] = korpa[i+1];
+                korpa[brojac] = null;
+                brojac--;
+                return povrat;
+            }
+        }
+        return null;
+    }
+    public int dajUkupnuCijenuArtikala () {
+        int suma = 0;
+        for (int i = 0; i < brojac; i++) suma = suma + korpa[i].getCijena();
+        return suma;
+    }
+
+    public Artikl[] getArtikli() {
+        return korpa;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] sve = new Artikl[1000];
+    private int brojac = 0;
+    public boolean dodajArtikl (Artikl a) {
+        if (brojac >= 50) return false;
+        sve[brojac] = new Artikl(a.getNaziv(),a.getCijena(),a.getKod());
+        brojac++;
+        return true;
+    }
+
+    public Artikl[] getSve() {
+        return sve;
+    }
+    public Artikl izbaciArtiklSaKodom (String kod) {
+        for (int i = 0; i < brojac; i++) {
+            if (sve[i].getKod().equals(kod))
+            {
+                Artikl povrat = new Artikl(sve[i].getNaziv(),sve[i].getCijena(),sve[i].getKod());
+                sve[i] = null;
+                for (int j = i; j < brojac; j++) sve[i] = sve[i+1];
+                sve[brojac] = null;
+                brojac--;
+                return povrat;
+            }
+        }
+        return null;
+    }
+
+    public Artikl[] getArtikli() {
+        return sve;
+    }
+
 }
